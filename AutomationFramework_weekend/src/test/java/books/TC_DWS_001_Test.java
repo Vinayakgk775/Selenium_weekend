f1bcd2d66f88e6caa5f3b3a455c77dcefd02bfaa
Vinayakgk775 <vinayakgk30@gmail.com>
2025-06-07 11:43:39 +0530

    Update TC_DWS_001_Test.java

--- AutomationFramework_weekend/src/test/java/books/TC_DWS_001_Test.java
+++ AutomationFramework_weekend/src/test/java/books/TC_DWS_001_Test.java
@@ -20,6 +20,7 @@ public class TC_DWS_001_Test extends BaseClass {
 		hp.getBooksLink().click();
 		Assert.assertEquals(driver.getCurrentUrl(),"https://demowebshop.tricentis.");
 		test.log(Status.PASS, "Books page is displayed");
+		test.log(Status.PASS, "Books page is displayed");
 	}
 	 
 
