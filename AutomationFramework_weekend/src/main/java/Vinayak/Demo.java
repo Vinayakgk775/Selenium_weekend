a00c9f148267f978816d5c6a1274510d8ff74732
Admin <Admin@DESKTOP-73UG3SV>
2025-06-07 14:48:45 +0530

    !st Commit

---
+++ AutomationFramework_weekend/src/main/java/Vinayak/Demo.java
@@ -0,0 +1,13 @@
+package Vinayak;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void launch() {
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
